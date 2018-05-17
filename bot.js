1be8a2c9a8c89d6bed161b93e0e0d2288d175a50
HenThaiPremium <39322995+HenThaiPremium@users.noreply.github.com>
2018-05-16 18:54:23 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
+});
+
+bot.login(process.env.NDQ2MTc5MjYyMTM1OTI2Nzk0.Dd5jcw.KvDLt_8_SL-boKX4FEo6BnVVOiA);
